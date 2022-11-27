c50ac6d1fb5b80d66ed8f3cb99bca472d373d310
Lucas Mocroski <devmocroski@gmail.com>
2022-11-27 09:12:55 -0300

    Criando o Model de Login

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -2,13 +2,16 @@ package servlets;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.annotation.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.ModelLogin;
 
+//o chamado Controller sao as servlets ou ServletsLoginController
 @WebServlet ("/ServetLogin")/*mapeamento de URL que vem da tela*/
 public class ServletLogin  extends HttpServlet {
 	
@@ -28,8 +31,22 @@ public class ServletLogin  extends HttpServlet {
 
 	
 	protected void doPost /*recebe os dadods enviado spor um formualiro*/(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println(request.getParameter("nome"));
-		System.out.println(request.getParameter("idade"));
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		if(login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+			
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+			
+		}else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Informe Login e senha corretamente!");
+			redirecionar.forward(request, response);
+		
+		}
+		
 	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -9,15 +9,31 @@
 <body>
 	<h1>Bem vindo</h1>
 
-	<%
-	out.print("teste jsp");
-	%>
 
-	<form action="/ServletLogin" method="post">
+	<form action="ServletLogin" method="post">
 
-		<input name="nome"> <input name="idade"> 
-		<input type="submit" value="Enviar">
+		<table>
+			<tr>
+				<td><label>Login</label></td>
+				<td><input name="login" type="text"></td>
+			</tr>
+
+			<tr>
+				<td><label>Senha</label></td>
+				<td><input name="senha" type="password"></td>
+			</tr>
+
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Enviar"></td>
+
+			</tr>
+		</table>
+
 	</form>
+
+<h4>${msg}</h4>
+
 
 </body>
 </html>
